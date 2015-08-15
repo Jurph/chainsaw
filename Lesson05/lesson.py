3a11f15917e7abb1f59a17974d0758b515cd05c3
czeano <czeano@users.noreply.github.com>
2015-08-14 17:00:38 -0700

    Add lesson.py to Lesson 05

---
+++ Lesson05/lesson.py
@@ -0,0 +1,11 @@
+
+things = ['dog', 'tree', 'house', 'rock', 'x-wing fighter', 'unicorn', ]
+animals = list()
+
+for item in things:
+    if item in ['bird', 'cat', ]:
+        animals.append(item)
+
+for critter in animals:
+    print(critter)
+
